8412e400dddaabefe81604b81e115b0acb95a445
Addison Schile <addschile@gmail.com>
2022-06-24 01:06:20 -0700

    added csc test

--- examples/run_test_use.sh
+++ examples/run_test_use.sh
@@ -8,6 +8,10 @@ echo "Running test_csr_use.cpp"
 $CXX -std=c++17 -I ../ test_csr_use.cpp -o sprun && ./sprun
 echo
 
+echo "Running test_csc_use.cpp"
+$CXX -std=c++17 -I ../ test_csc_use.cpp -o sprun && ./sprun
+echo
+
 export BOOST=$HOME/programming/boost_1_79_0/
 echo "Running test_dok_use.cpp"
 $CXX -std=c++17 -I ../ -I $BOOST test_dok_use.cpp -o sprun && ./sprun
---
+++ examples/test_csc_use.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <spla/csc/csc_matrix>
+
+int main()
+{
+    auto spmat   = SPLA::CSCMatrix<double>(8, 10, 6);
+    auto nrows   = spmat.get_rows();
+    auto ncols   = spmat.get_cols();
+    auto nvalues = spmat.get_size();
+    std::cout << "The number of rows is "   << nrows   << std::endl;
+    std::cout << "The number of cols is "   << ncols   << std::endl;
+    std::cout << "The number of values is " << nvalues << std::endl;
+    std::cout << "The data is currently:"              << std::endl;
+    for (size_t i=0; i<nvalues; i++){
+        std::cout << spmat[i] << std::endl;
+        spmat[i] = i;
+    }
+
+    std::cout << "Copying the matrix" << std::endl;
+    auto new_spmat   = SPLA::CSCMatrix<double>(spmat);
+    auto new_nrows   = new_spmat.get_rows();
+    auto new_ncols   = new_spmat.get_cols();
+    auto new_nvalues = new_spmat.get_size();
+    std::cout << "The number of rows is "   << new_nrows   << std::endl;
+    std::cout << "The number of cols is "   << new_ncols   << std::endl;
+    std::cout << "The number of values is " << new_nvalues << std::endl;
+    std::cout << "The data is currently:"                  << std::endl;
+    for (size_t i=0; i<nvalues; i++){
+        std::cout << new_spmat[i] << std::endl;
+    }
+
+    return 0;
+}
