3dc6e54e2f0842c39a7fe2f5e389742fbf48a9bf
Addison Schile <addschile@gmail.com>
2022-06-23 21:55:47 -0700

    quick check that the copy constructor works

--- examples/test_csr_use.cpp
+++ examples/test_csr_use.cpp
@@ -13,6 +13,20 @@ int main()
     std::cout << "The data is currently:"              << std::endl;
     for (size_t i=0; i<nvalues; i++){
         std::cout << spmat[i] << std::endl;
+        spmat[i] = i;
+    }
+
+    std::cout << "Copying the matrix" << std::endl;
+    auto new_spmat   = SPLA::CSRMatrix<double>(spmat);
+    auto new_nrows   = new_spmat.get_rows();
+    auto new_ncols   = new_spmat.get_cols();
+    auto new_nvalues = new_spmat.get_size();
+    std::cout << "The number of rows is "   << new_nrows   << std::endl;
+    std::cout << "The number of cols is "   << new_ncols   << std::endl;
+    std::cout << "The number of values is " << new_nvalues << std::endl;
+    std::cout << "The data is currently:"                  << std::endl;
+    for (size_t i=0; i<nvalues; i++){
+        std::cout << new_spmat[i] << std::endl;
     }
 
     return 0;
