7086c56b56f88d56a19c330d810529f8768d997b
Addison Schile <addschile@gmail.com>
2022-06-23 21:54:25 -0700

    added copy constructor for csr matrix

--- spla/csr/csr_matrix.cpp
+++ spla/csr/csr_matrix.cpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <algorithm>
 
 namespace SPLA {
@@ -14,6 +15,18 @@ CSRMatrix<Numeric>::CSRMatrix(const std::size_t nrow, const std::size_t ncol, co
     this->set_cols(ncol);
     this->set_size(nvalues);
     this->allocate();
+}
+
+template<typename Numeric>
+CSRMatrix<Numeric>::CSRMatrix(const CSRMatrix& spmat)
+{
+    this->set_rows(spmat.get_rows());
+    this->set_cols(spmat.get_cols());
+    this->set_size(spmat.get_size());
+    this->allocate();
+    for (int i=0; i<this->get_size(); i++){
+        (this->data)[i] = spmat[i];
+    }
 }
 
 template<typename Numeric>
