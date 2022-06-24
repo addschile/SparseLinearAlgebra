9cc9e348afca9c9b9b3f185ed5e53bbd4e58108b
Addison Schile <addschile@gmail.com>
2022-06-23 20:56:56 -0700

    refactored compiler for easy swapping and testing

--- examples/run_test_use.sh
+++ examples/run_test_use.sh
@@ -1,12 +1,14 @@
+export CXX=g++-12
+
 echo "Running test_use.cpp"
-g++ -std=c++17 -I ../src/ test_use.cpp -o sprun && ./sprun
+$CXX -std=c++17 -I ../src/ test_use.cpp -o sprun && ./sprun
 echo
 
 echo "Running test_csr_use.cpp"
-g++ -std=c++17 -I ../src/ test_csr_use.cpp -o sprun && ./sprun
+$CXX -std=c++17 -I ../src/ test_csr_use.cpp -o sprun && ./sprun
 echo
 
 export BOOST=$HOME/programming/boost_1_79_0/
 echo "Running test_dok_use.cpp"
-g++ -std=c++17 -I ../src/ -I $BOOST test_dok_use.cpp -o sprun && ./sprun
+$CXX -std=c++17 -I ../src/ -I $BOOST test_dok_use.cpp -o sprun && ./sprun
 echo
